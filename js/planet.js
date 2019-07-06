28d0cc6c1c8a6171442b395bb07e4848865ff9b3
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2019-07-06 17:23:11 +0700

    added js file that contain a sample info about mercuryand config a little bit on css

---
+++ js/planet.js
@@ -0,0 +1,139 @@
+var planet = [
+    {
+        name: 'Mercury',
+        imgurl: '../img/planet/mercury.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Venus',
+        imgurl: '../img/planet/venus.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Earth',
+        imgurl: '../img/planet/earth.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Mars',
+        imgurl: '../img/planet/mars.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Jupiter',
+        imgurl: '../img/planet/jupiter.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Saturn',
+        imgurl: '../img/planet/saturn.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Uranus',
+        imgurl: '../img/planet/uranus.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }, {
+        name: 'Neptune',
+        imgurl: '../img/planet/neptune.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
+    }
+];
