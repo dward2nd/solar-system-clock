1de89dfd06a63012d102be75948a445c7747526b
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2019-07-06 18:16:39 +0700

    change a lot

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var orbit = [
+    document.querySelector(".orbit#mercury"),
+    document.querySelector(".orbit#venus"),
+    document.querySelector(".orbit#earth"),
+    document.querySelector(".orbit#mars"),
+    document.querySelector(".orbit#jupiter"),
+    document.querySelector(".orbit#saturn"),
+    document.querySelector(".orbit#uranus"),
+    document.querySelector(".orbit#neptune"),
+    document.querySelector(".orbit#sun"),
+];
+
+document.addEventListener("load", function() {
+
+});
--- js/planet.js
+++ js/planet.js
@@ -135,5 +135,22 @@ var planet = [
         rings: false, // does it have moon?
         tempmin: -173, // deg c
         tempmax: 427 // deg c
+    }, {
+        name: 'Sun',
+        imgurl: '../img/planet/sun.jpg',
+        tiltPeriod: 58.646, // days per period
+        tiltAngle: 0, // degree
+        orbitPeriod: 0.2408467, // years
+        orbitAngle: 0, // degree note: add later
+        orbitRadius: 5.7909227e7, // kilometers
+        volume: 6.0827208742e10, // km^3
+        mass: 3.30104e23, // kg
+        density: 5.427, // g/cm^3
+        gravity: 3.7, // m/s^2
+        escape: 1.5e4, // km/h
+        moon: 0, // num of satellites
+        rings: false, // does it have moon?
+        tempmin: -173, // deg c
+        tempmax: 427 // deg c
     }
 ];
