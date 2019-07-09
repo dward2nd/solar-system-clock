54224603702229fb4942ee777b0bea3abf093a02
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2019-07-09 16:55:27 +0700

    add tooltip

--- js/onclicksetspeed.js
+++ js/onclicksetspeed.js
@@ -1,11 +1,15 @@
-var setSpeedBtn = document.getElementById('setSpeed');
+// var setSpeedBtn = document.getElementById('setSpeed');
 var speedTxtBox = document.getElementById('speed');
 
-setSpeedBtn.addEventListener('click', function() {
+speedTxtBox.addEventListener('input', function() {
     var multipier = parseInt(speedTxtBox.value);
+
     for(i=0; i<8; i++) {
-        orbitEl[i].style.animationPlayState = 'paused';
-        orbitEl[i].style.animationDuration = (planet[i].orbitPeriod / multipier) + 's';
-        orbitEl[i].style.animationPlayState = 'running';
+        if(multipier == 0) {
+            orbitEl[i].style.animationPlayState = 'paused';
+        } else {
+            orbitEl[i].style.animationDuration = (planet[i].orbitPeriod / multipier) + 's';
+            orbitEl[i].style.animationPlayState = 'running';
+        }
     }
 });
